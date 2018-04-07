c19e30981300b70e91366bd15271f56e91d0d6a6
Dschorim <Dschorim@users.noreply.github.com>
2018-04-07 10:49:17 +0000

    quickfix

--- install_dont_execute.sh
+++ install_dont_execute.sh
@@ -4,7 +4,7 @@ git clone -b xtensa-1.22.x https://github.com/espressif/crosstool-NG.git
 cd crosstool-NG
 ./bootstrap && ./configure --enable-local && make install
 ./ct-ng xtensa-esp32-elf
-#./ct-ng build build.1
-#chmod -R u+w builds/xtensa-esp32-elf
-#cp -r $HOME/esp/crosstool-NG/builds/xtensa-esp32-elf/ $HOME/esp/xtensa-rpi
-#rm -r $HOME/esp/crosstool-NG/
+./ct-ng build build.1
+chmod -R u+w builds/xtensa-esp32-elf
+cp -r $HOME/esp/crosstool-NG/builds/xtensa-esp32-elf/ $HOME/esp/xtensa-rpi
+rm -r $HOME/esp/crosstool-NG/
